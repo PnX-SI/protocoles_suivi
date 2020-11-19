a4afcefec82bcc48ff4a902d83fbda84c30064ee
joel.clement <joelclems@gmail.com>
2020-11-19 14:13:44 +0100

    cheveches group

---
+++ cheveches_sites_group/synthese.sql
@@ -0,0 +1,133 @@
+DROP VIEW IF EXISTS gn_monitoring.v_synthese_cheveches;
+CREATE VIEW gn_monitoring.v_synthese_cheveches AS
+
+WITH source AS (
+
+	SELECT
+
+        id_source
+
+    FROM gn_synthese.t_sources
+	WHERE name_source = CONCAT('MONITORING_', UPPER('cheveches'))
+	LIMIT 1
+
+), sites AS (
+
+    SELECT
+
+        id_base_site,
+        uuid_base_site,
+        geom AS the_geom_4326,
+	    ST_CENTROID(geom) AS the_geom_point,
+	    geom_local as geom_local
+
+        FROM gn_monitoring.t_base_sites
+
+), visits AS (
+    
+    SELECT
+    
+        id_base_visit,
+        uuid_base_visit,
+        id_module,
+        id_base_site,
+        id_dataset,
+        id_digitiser,
+
+        visit_date_min AS date_min,
+	    COALESCE (visit_date_max, visit_date_min) AS date_max,
+        comments,
+
+    	id_nomenclature_tech_collect_campanule,
+	    id_nomenclature_grp_typ
+
+        FROM gn_monitoring.t_base_visits
+
+), observers AS (
+    SELECT
+        array_agg(r.id_role) AS ids_observers,
+        STRING_AGG(CONCAT(r.nom_role, ' ', prenom_role), ' ; ') AS observers,
+        id_base_visit
+    FROM gn_monitoring.cor_visit_observer cvo
+    JOIN utilisateurs.t_roles r
+    ON r.id_role = cvo.id_role
+    GROUP BY id_base_visit
+)
+
+
+SELECT
+		
+		v.uuid_base_visit AS unique_id_sinp, 
+		s.uuid_base_site AS unique_id_sinp_grp,
+		source.id_source,
+		v.id_base_visit AS entity_source_pk_value,
+		v.id_dataset,
+        ref_nomenclatures.get_id_nomenclature('NAT_OBJ_GEO', 'St') AS id_nomenclature_geo_object_nature,
+
+		v.id_nomenclature_grp_typ, -- TYP_GRP
+		--id_nomen clature_obs_technique, -- METH_OBS
+		v.id_nomenclature_tech_collect_campanule, --TECHNIQUE_OBS
+		--id_nomenclature_bio_status, -- STATUT_BIO
+		--id_nomenclature_bio_condition, -- ETA_BIO
+		--id_nomenclature_naturalness, -- NATURALITE
+		--id_nomenclature_exist_proof, -- PREUVE_EXIST
+		--id_nomenclature_valid_status,  --STATUT_VALID
+		--id_nomenclature_diffusion_level, -- NIV_PRECIS
+		--id_nomenclature_life_stage, -- STADE_VIE
+		--id_nomenclature_sex, -- SEXE
+ 		ref_nomenclatures.get_id_nomenclature('IND', 'OBJ_DENBR') AS id_nomenclature_obj_count,
+ 		ref_nomenclatures.get_id_nomenclature('TYP_DENBR', 'Es') AS id_nomenclature_type_count,
+ 		-- id_nomenclature_sensitivity, --SENSIBILITE
+ 		ref_nomenclatures.get_id_nomenclature('STATUT_OBS', 'Pr') AS id_nomenclature_observation_status, 
+		-- id_nomenclature_blurring, -- DEE_FLOU
+        -- id_nomenclature_behaviour, -- OCC_COMPORTEMENT
+		ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE', 'Te') AS id_nomenclature_source_status,
+		ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO', '1') AS id_nomenclature_info_geo_type,
+
+		1 AS count_min,
+		1 AS count_max,
+		t.cd_nom,
+		t.nom_complet AS nom_cite,
+		--meta_v_taxref
+		--sample_number_proof
+		--digital_proofvue
+		alt.altitude_min,
+		alt.altitude_max,
+		s.the_geom_4326,
+		s.the_geom_point,
+		s.geom_local as the_geom_local,
+		v.date_min,
+		v.date_max,
+		--validator
+		--validation_comment
+		obs.observers,
+		--determiner
+		v.id_digitiser,
+		--id_nomenclature_determination_method
+		--meta_validation_date
+		--meta_create_date,
+		--meta_update_date,
+		--last_action
+		v.id_module,
+		v.comments AS comment_description,
+		obs.ids_observers,
+		
+		-- ## Colonnes complémentaires qui ont leur utilité dans la fonction synthese.import_row_from_table
+		v.id_base_site,
+		v.id_base_visit
+
+    FROM visits v
+    JOIN sites s 
+        ON s.id_base_site = v.id_base_site
+	JOIN gn_commons.t_modules m 
+        ON m.id_module = v.id_module
+	JOIN gn_monitoring.t_visit_complements vc ON vc.id_base_visit = v.id_base_visit
+	JOIN ref_nomenclatures.t_nomenclatures n ON n.id_nomenclature = (vc.data->>'id_nomenclature_statut_obs')::int
+	JOIN taxonomie.taxref t ON cd_nom = (vc.data->>'cd_nom')::int
+	JOIN observers obs ON obs.id_base_visit = v.id_base_visit
+	JOIN source 
+        ON TRUE
+ 	LEFT JOIN LATERAL ref_geo.fct_get_altitude_intersection(s.geom_local) alt (altitude_min, altitude_max)
+        ON TRUE
+    WHERE m.module_code = 'cheveches'
+    ;
