2ece19dc9c4926604c65c6a83059f7d8bdfe86d0
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2022-03-11 16:09:48 +0100

    Update apollons

--- apollons/synthese.sql
+++ apollons/synthese.sql
@@ -2,15 +2,11 @@ DROP VIEW IF EXISTS gn_monitoring.v_synthese_apollons;
 CREATE VIEW gn_monitoring.v_synthese_apollons AS
 
 WITH source AS (
-
-	SELECT
-
+    SELECT
         id_source
-
     FROM gn_synthese.t_sources
-	WHERE name_source = CONCAT('MONITORING_', UPPER('apollons'))
-	LIMIT 1
-
+    WHERE name_source = CONCAT('MONITORING_', UPPER('apollons'))
+    LIMIT 1
 ), observers AS (
     SELECT
         array_agg(r.id_role) AS ids_observers,
@@ -35,8 +31,16 @@ WITH source AS (
     ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE'::character varying, 'Te'::character varying) AS id_nomenclature_source_status,
     ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO'::character varying, '1'::character varying) AS id_nomenclature_info_geo_type,
     ref_nomenclatures.get_id_nomenclature('STADE_VIE'::character varying, '7'::character varying) AS id_nomenclature_life_stage,
-    1 AS count_min,
-    1 AS count_max,
+    COALESCE((vc.DATA->>'count_stade_l1')::int, 0) +
+    COALESCE((vc.DATA->>'count_stade_l2')::int, 0) +
+    COALESCE((vc.DATA->>'count_stade_l3')::int, 0) +
+    COALESCE((vc.DATA->>'count_stade_l4')::int, 0) +
+    COALESCE((vc.DATA->>'count_stade_l5')::int, 0)  AS count_min,
+    COALESCE((vc.DATA->>'count_stade_l1')::int, 0) +
+    COALESCE((vc.DATA->>'count_stade_l2')::int, 0) +
+    COALESCE((vc.DATA->>'count_stade_l3')::int, 0) +
+    COALESCE((vc.DATA->>'count_stade_l4')::int, 0) +
+    COALESCE((vc.DATA->>'count_stade_l5')::int, 0)  AS count_max,
     t.cd_nom,
     t.nom_complet AS nom_cite,
     alt.altitude_min,
@@ -57,9 +61,15 @@ WITH source AS (
      JOIN gn_monitoring.t_base_sites s ON s.id_base_site = v.id_base_site
      JOIN gn_commons.t_modules m ON m.id_module = v.id_module
      JOIN gn_monitoring.t_visit_complements vc ON vc.id_base_visit = v.id_base_visit
-     JOIN ref_nomenclatures.t_nomenclatures n ON n.id_nomenclature = ((vc.data ->> 'id_nomenclature_statut_obs'::text)::integer) AND n.cd_nomenclature::text = 'Pr'::text
+     --JOIN ref_nomenclatures.t_nomenclatures n ON n.id_nomenclature = ((vc.data ->> 'id_nomenclature_statut_obs'::text)::integer) AND n.cd_nomenclature::text = 'Pr'::text
      JOIN taxonomie.taxref t ON t.cd_nom = ((vc.data ->> 'cd_nom'::text)::integer)
      LEFT JOIN observers obs ON obs.id_base_visit = v.id_base_visit
      JOIN source ON true
      LEFT JOIN LATERAL ref_geo.fct_get_altitude_intersection(s.geom_local) alt(altitude_min, altitude_max) ON true
-  WHERE m.module_code::text = 'apollons'::text;
+  WHERE m.module_code::text = 'apollons'::TEXT
+    AND COALESCE((vc.DATA->>'count_stade_l1')::int, 0) +
+    COALESCE((vc.DATA->>'count_stade_l2')::int, 0) +
+    COALESCE((vc.DATA->>'count_stade_l3')::int, 0) +
+    COALESCE((vc.DATA->>'count_stade_l4')::int, 0) +
+    COALESCE((vc.DATA->>'count_stade_l5')::int, 0)  > 0
+  ;
