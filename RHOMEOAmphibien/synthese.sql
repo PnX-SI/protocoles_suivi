3df64771e6710992fc6acea94ba7941c3c5598b1
Colas Geier <colas.geier@cen-isere.org>
2022-12-08 09:52:00 +0100

    update module amphibien

--- RHOMEOAmphibien/synthese.sql
+++ RHOMEOAmphibien/synthese.sql
@@ -19,8 +19,8 @@
 -- ne pas remplacer cette variable, elle est indispensable pour les scripts d'installations
 -- le module pouvant être installé avec un code différent de l'original
 
-DROP VIEW IF EXISTS gn_monitoring.v_synthese_RHOMEOAmphibien;
-CREATE VIEW gn_monitoring.v_synthese_RHOMEOAmphibien AS
+DROP VIEW IF EXISTS gn_monitoring.v_synthese_:module_code;
+CREATE VIEW gn_monitoring.v_synthese_:module_code AS
 
 WITH source AS (
 
@@ -29,7 +29,7 @@ WITH source AS (
         id_source
 
     FROM gn_synthese.t_sources
-	WHERE name_source = CONCAT('MONITORING_', UPPER('RHOMEOAmphibien'))
+	WHERE name_source = CONCAT('MONITORING_', UPPER(:'module_code'))
 	LIMIT 1
 
 ), sites AS (
@@ -39,9 +39,32 @@ WITH source AS (
         id_base_site,
         geom AS the_geom_4326,
 	    ST_CENTROID(geom) AS the_geom_point,
-	    geom_local as the_geom_local
+	    geom_local as the_geom_local,
+		id_nomenclature_type_site,
+		sg.sites_group_name,
+		sg.sites_group_description,
+		base_site_name,
+		altitude_min,
+		ref_nomenclatures.get_nomenclature_label((sc.data::json#>>'{prospect_typo}')::int) AS prospect_typo,
+		ref_nomenclatures.get_nomenclature_label((sc.data::json#>>'{prospect_form}')::int) AS prospect_form,
+		(sc.data::json#>>'{profondeur_maxi}')::text AS profondeur_maxi,
+		(sc.data::json#>>'{ombrage}')::text AS ombrage,
+		(sc.data::json#>>'{type_pente}')::text AS type_pente,
+		(SELECT 
+		 	array_agg(ref_nomenclatures.get_nomenclature_label(sub2.val2::int))
+			FROM (SELECT jsonb_array_elements(sb.val) val2 FROM (VALUES (sc."data"#>'{substrat}')) as sb(val)
+		) as sub2) AS substrat, -- list
+		ref_nomenclatures.get_nomenclature_label((sc.data::json#>>'{origine_eau}')::int) AS origine_eau,
+		ref_nomenclatures.get_nomenclature_label((sc.data::json#>>'{fuite_eau}')::int) AS fuite_eau,
+		ref_nomenclatures.get_nomenclature_label((sc.data::json#>>'{context_pays}')::int) AS context_pays,
+		(sc.data::json#>>'{esp_veget}')::text AS esp_veget,
+		(sc.data::json#>>'{esp_poisson}')::text AS esp_poisson,
+		(sc.data::json#>>'{esp_ecrevisse}')::text AS esp_ecrevisse,
+		base_site_description
 
         FROM gn_monitoring.t_base_sites
+		JOIN gn_monitoring.t_site_complements sc USING (id_base_site)
+		LEFT JOIN gn_monitoring.t_sites_groups sg USING (id_sites_group)
 
 ), visits AS (
     
@@ -54,8 +77,13 @@ WITH source AS (
         id_dataset,
         id_digitiser,
 
-        visit_date_min AS date_min,
-	    COALESCE (visit_date_max, visit_date_min) AS date_max,
+        (visit_date_min||' '||(vc.data::json#>>'{Heure_debut}')::text||'\:00')::timestamp AS date_min,
+	    (COALESCE (visit_date_max, visit_date_min)||' '||(vc.data::json#>>'{Heure_debut}')::text||'\:00')::timestamp AS date_max,
+		((vc.data::json#>>'{Heure_debut}')::text||'\:00')::time AS "Heure_debut",
+		(vc.data::json#>>'{num_passage}')::int AS num_visit,
+		(vc.data::json#>>'{surf_eau}')::text AS surf_eau_m2,
+		(vc.data::json#>>'{color_eau}')::text AS color_eau,
+		ref_nomenclatures.get_nomenclature_label((vc.data::json#>>'{transparence_eau}')::int) AS transparence_eau,
         comments,
 
 	    --o.observers,
@@ -65,6 +93,7 @@ WITH source AS (
 	    id_nomenclature_grp_typ
 
         FROM gn_monitoring.t_base_visits
+		JOIN gn_monitoring.t_visit_complements vc USING (id_base_visit)
 
 ), observers AS (
     SELECT
@@ -95,25 +124,29 @@ SELECT
 		--oc.id_nomenclature_life_stage, -- STADE_VIE
 		--id_nomenclature_sex, -- SEXE
  		ref_nomenclatures.get_id_nomenclature('OBJ_DENBR','IND') AS id_nomenclature_obj_count,
- 		ref_nomenclatures.get_id_nomenclature('TYP_DENBR', 'Es') AS id_nomenclature_type_count,
+		CASE WHEN (oc."data"->>'nombre_compte') IS NULL THEN 
+ 			ref_nomenclatures.get_id_nomenclature('TYP_DENBR', 'Es')
+		ELSE
+ 			ref_nomenclatures.get_id_nomenclature('TYP_DENBR', 'Co')
+		END AS id_nomenclature_type_count,
 		ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE', 'Te') AS id_nomenclature_source_status,
 		ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO', '1') AS id_nomenclature_info_geo_type,
 		ref_nomenclatures.get_id_nomenclature('TYP_GRP', 'REL') as id_nomenclature_grp_typ, 
     	ref_nomenclatures.get_id_nomenclature('NIV_PRECIS', '4') as id_nomenclature_diffusion_level, -- pas de diffusion pour l'instant (en attente des règles),
  		ref_nomenclatures.get_id_nomenclature('STATUT_OBS', 'Pr') AS id_nomenclature_observation_status, 
-		CASE WHEN (oc.data->>'nombre_compte') IS NULL THEN SPLIT_PART(REPLACE((oc.data->>'nombre')::text,'> ','')::text,' à ',1)::int
-			ELSE (oc.data->>'nombre_compte')::int
+		CASE WHEN (oc."data"->>'nombre_compte') IS NULL THEN SPLIT_PART(REPLACE((oc."data"->>'nombre')::text,'> ','')::text,' à ',1)::int
+			ELSE (oc."data"->>'nombre_compte')::int
 		END AS count_min,
-		CASE WHEN (oc.data->>'nombre_compte') IS NULL THEN 
-			CASE WHEN SPLIT_PART(REPLACE((oc.data->>'nombre')::text,'> ','')::text,' à ',2) = '' THEN 9999
-				ELSE SPLIT_PART(REPLACE((oc.data->>'nombre')::text,'> ','')::text,' à ',2)::int
+		CASE WHEN (oc."data"->>'nombre_compte') IS NULL THEN 
+			CASE WHEN SPLIT_PART(REPLACE((oc."data"->>'nombre')::text,'> ','')::text,' à ',2) = '' THEN 9999
+				ELSE SPLIT_PART(REPLACE((oc."data"->>'nombre')::text,'> ','')::text,' à ',2)::int
 			END
-			ELSE (oc.data->>'nombre_compte')::int
+			ELSE (oc."data"->>'nombre_compte')::int
 		END AS count_max,
 		id_observation,    
 		t.cd_nom,
     	t.nom_complet AS nom_cite,
-		(oc."data"->>'id_nomenclature_life_stage')::integer AS id_nomenclature_life_stage,
+		(oc."data"->>'id_nomenclature_life_stage')::int AS id_nomenclature_life_stage,
 		--meta_v_taxref
 		--sample_number_proof
 		--digital_proofvue
@@ -123,7 +156,31 @@ SELECT
 		s.the_geom_point,
 		s.the_geom_local,
 		v.date_min,
-		v.date_max,
+		v.date_max + ((oc."data"->>'duree_peche')||' minutes')::interval date_max,
+		jsonb_build_object(
+			'prospect_typo',s.prospect_typo,
+			'prospect_form',s.prospect_form,
+			'profondeur_maxi',s.profondeur_maxi,
+			'ombrage',s.ombrage,
+			'type_pente',s.type_pente,
+			'substrat',s.substrat,
+			'origine_eau',s.origine_eau,
+			'fuite_eau',s.fuite_eau,
+			'context_pays',s.context_pays,
+			'esp_veget',s.esp_veget,
+			'esp_poisson',s.esp_poisson,
+			'esp_ecrevisse',s.esp_ecrevisse,
+			'Heure_debut',v."Heure_debut",
+			'num_visit',v.num_visit,
+			'surf_eau_m2',v.surf_eau_m2,
+			'color_eau',v.color_eau,
+			'transparence_eau',v.transparence_eau,
+			'detection',(oc."data"::json#>>'{typ_detection}')::text,
+			'duree_peche',(oc."data"::json#>>'{duree_peche}')::text,
+			'effectif', CASE 
+				WHEN (oc."data"::json#>>'{nombre_compte}') IS NULL THEN (oc."data"::json#>>'{nombre}')
+				ELSE (oc."data"::json#>>'{nombre_compte}') END
+		) additional_data,
 		--validator
 		--validation_comment
 		obs.observers,
@@ -138,14 +195,11 @@ SELECT
 		v.comments AS comment_context,
 		o.comments AS comment_description,
 		obs.ids_observers,
-		
 		-- ## Colonnes complémentaires qui ont leur utilité dans la fonction synthese.import_row_from_table
 		v.id_base_site,
 		v.id_base_visit
-
     FROM gn_monitoring.t_observations o 
 	JOIN gn_monitoring.t_observation_complements oc USING (id_observation)
-
     JOIN visits v
         ON v.id_base_visit = o.id_base_visit
     JOIN sites s 
@@ -156,9 +210,9 @@ SELECT
         ON t.cd_nom = o.cd_nom
 	JOIN source 
         ON TRUE
-	JOIN observers obs ON obs.id_base_visit = v.id_base_visit
-    
+	JOIN observers obs 
+		ON obs.id_base_visit = v.id_base_visit
  	LEFT JOIN LATERAL ref_geo.fct_get_altitude_intersection(s.the_geom_local) alt (altitude_min, altitude_max)
         ON TRUE
-    WHERE m.module_code = 'RHOMEOAmphibien'
+    WHERE m.module_code = :'module_code'
     ;
