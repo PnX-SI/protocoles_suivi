db4a17afc823e2cec57671a70b2db1ecc6fc239f
LpoAuRADev <webadmin.aura@lpo.fr>
2024-06-21 17:00:13 +0200

    intégration dernière modifs modules

--- nichoirs/synthese.sql
+++ nichoirs/synthese.sql
@@ -19,141 +19,87 @@
 -- ne pas remplacer cette variable, elle est indispensable pour les scripts d'installations
 -- le module pouvant être installé avec un code différent de l'original
 
-DROP VIEW IF EXISTS gn_monitoring.v_synthese_POPAmphibien;
-CREATE VIEW gn_monitoring.v_synthese_POPAmphibien AS
-
-WITH source AS (
-
-	SELECT
-
-        id_source
-
-    FROM gn_synthese.t_sources
-	WHERE name_source = CONCAT('MONITORING_', UPPER('POPAmphibien'))
-	LIMIT 1
-
-), sites AS (
-
-    SELECT
-
-        id_base_site,
-        geom AS the_geom_4326,
-	    ST_CENTROID(geom) AS the_geom_point,
-	    geom_local as geom_local
-
-        FROM gn_monitoring.t_base_sites
-
-), visits AS (
-    
-    SELECT
-    
-        id_base_visit,
-        uuid_base_visit,
-        id_module,
-        id_base_site,
-        id_dataset,
-        id_digitiser,
-
-        visit_date_min AS date_min,
-	    COALESCE (visit_date_max, visit_date_min) AS date_max,
-        comments,
-
-	    --o.observers,
-	    --o.ids_observers,
-
-    	id_nomenclature_tech_collect_campanule,
-	    id_nomenclature_grp_typ
-
-        FROM gn_monitoring.t_base_visits
-
-), observers AS (
-    SELECT
-        array_agg(r.id_role) AS ids_observers,
-        STRING_AGG(CONCAT(r.nom_role, ' ', prenom_role), ' ; ') AS observers,
-        id_base_visit
-    FROM gn_monitoring.cor_visit_observer cvo
-    JOIN utilisateurs.t_roles r
-    ON r.id_role = cvo.id_role
-    GROUP BY id_base_visit
-)
-
-SELECT
-		
-        o.uuid_observation AS unique_id_sinp, 
-		v.uuid_base_visit AS unique_id_sinp_grp,
-		source.id_source,
-		o.id_observation AS entity_source_pk_value,
-		v.id_dataset,
-        ref_nomenclatures.get_id_nomenclature('NAT_OBJ_GEO', 'St') AS id_nomenclature_geo_object_nature,
-
-		v.id_nomenclature_grp_typ, -- TYP_GRP
-		--id_nomen clature_obs_technique, -- METH_OBS
-		v.id_nomenclature_tech_collect_campanule, --TECHNIQUE_OBS
-		--id_nomenclature_bio_status, -- STATUT_BIO
-		--id_nomenclature_bio_condition, -- ETA_BIO
-		--id_nomenclature_naturalness, -- NATURALITE
-		--id_nomenclature_exist_proof, -- PREUVE_EXIST
-		--id_nomenclature_valid_status,  --STATUT_VALID
-		--id_nomenclature_diffusion_level, -- NIV_PRECIS
-		--id_nomenclature_life_stage, -- STADE_VIE
-		--id_nomenclature_sex, -- SEXE
- 		ref_nomenclatures.get_id_nomenclature('OBJ_DENBR','IND') AS id_nomenclature_obj_count,
- 		ref_nomenclatures.get_id_nomenclature('TYP_DENBR', 'Es') AS id_nomenclature_type_count,
- 		-- id_nomenclature_sensitivity, --SENSIBILITE
- 		ref_nomenclatures.get_id_nomenclature('STATUT_OBS', 'Pr') AS id_nomenclature_observation_status, 
-		-- id_nomenclature_blurring, -- DEE_FLOU
-        -- id_nomenclature_behaviour, -- OCC_COMPORTEMENT
-		ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE', 'Te') AS id_nomenclature_source_status,
-		ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO', '1') AS id_nomenclature_info_geo_type,
-
-		1 AS count_min,
-		1 AS count_max,
-		id_observation,
-		o.cd_nom,
-		t.nom_complet AS nom_cite,
-		--meta_v_taxref
-		--sample_number_proof
-		--digital_proofvue
-		alt.altitude_min,
-		alt.altitude_max,
-		s.the_geom_4326,
-		s.the_geom_point,
-		s.geom_local as the_geom_local,
-		v.date_min,
-		v.date_max,
-		--validator
-		--validation_comment
-		obs.observers,
-		--determiner
-		v.id_digitiser,
-		--id_nomenclature_determination_method
-		--meta_validation_date
-		--meta_create_date,
-		--meta_update_date,
-		--last_action
-		v.id_module,
-		v.comments AS comment_context,
-		o.comments AS comment_description,
-		obs.ids_observers,
-		
-		-- ## Colonnes complémentaires qui ont leur utilité dans la fonction synthese.import_row_from_table
-		v.id_base_site,
-		v.id_base_visit
-
-    FROM gn_monitoring.t_observations o
-    JOIN visits v
-        ON v.id_base_visit = o.id_base_visit
-    JOIN sites s 
-        ON s.id_base_site = v.id_base_site
-	JOIN gn_commons.t_modules m 
-        ON m.id_module = v.id_module
-	JOIN taxonomie.taxref t 
-        ON t.cd_nom = o.cd_nom
-	JOIN source 
-        ON TRUE
-	JOIN observers obs ON obs.id_base_visit = v.id_base_visit
-    
- 	LEFT JOIN LATERAL ref_geo.fct_get_altitude_intersection(s.geom_local) alt (altitude_min, altitude_max)
-        ON TRUE
-    WHERE m.module_code = 'POPAmphibien'
-    ;
+DROP VIEW IF EXISTS gn_monitoring.v_synthese_nichoirs;
+CREATE OR REPLACE VIEW gn_monitoring.v_synthese_nichoirs
+AS WITH t_source AS (
+         SELECT t_sources.id_source
+           FROM gn_synthese.t_sources
+          WHERE t_sources.name_source::text = concat('MONITORING_', upper('NICHOIRS'::text))
+        ), observers AS (
+         SELECT array_agg(r.id_role) AS ids_observers,
+            string_agg(concat(r.nom_role, ' ', r.prenom_role), ' ; '::text) AS observers,
+            cvo.id_base_visit
+           FROM gn_monitoring.cor_visit_observer cvo
+             JOIN utilisateurs.t_roles r ON r.id_role = cvo.id_role
+          GROUP BY cvo.id_base_visit
+        )
+ SELECT to2.uuid_observation AS unique_id_sinp,
+    tbs.uuid_base_site AS unique_id_sinp_grp,
+    t_source.id_source,
+    tsc.id_module,
+    to2.id_observation AS entity_source_pk_value,
+    tbv.id_dataset,
+    ref_nomenclatures.get_id_nomenclature('NAT_OBJ_GEO'::character varying, 'St'::character varying) AS id_nomenclature_geo_object_nature,
+    ref_nomenclatures.get_id_nomenclature('OBJ_DENBR'::character varying, 'IND'::character varying) AS id_nomenclature_obj_count,
+    ref_nomenclatures.get_id_nomenclature('TYP_DENBR'::character varying, 'Co'::character varying) AS id_nomenclature_type_count,
+    ref_nomenclatures.get_id_nomenclature('STATUT_OBS'::character varying, 'Pr'::character varying) AS id_nomenclature_observation_status,
+    ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE'::character varying, 'Te'::character varying) AS id_nomenclature_source_status,
+    ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO'::character varying, '1'::character varying) AS id_nomenclature_info_geo_type,
+        CASE
+            WHEN (toc.data #>> '{adulte}'::text[]) ~~* ''::text OR (toc.data #>> '{adulte}'::text[]) IS NULL THEN 0
+            ELSE (toc.data #>> '{adulte}'::text[])::integer
+        END +
+        CASE
+            WHEN (toc.data #>> '{oeuf}'::text[]) ~~* ''::text OR (toc.data #>> '{oeuf}'::text[]) IS NULL THEN 0
+            ELSE (toc.data #>> '{oeuf}'::text[])::integer
+        END +
+        CASE
+            WHEN (toc.data #>> '{jeune_vivant}'::text[]) ~~* ''::text OR (toc.data #>> '{jeune_vivant}'::text[]) IS NULL THEN 0
+            ELSE (toc.data #>> '{jeune_vivant}'::text[])::integer
+        END +
+        CASE
+            WHEN (toc.data #>> '{jeune_mort}'::text[]) ~~* ''::text OR (toc.data #>> '{jeune_mort}'::text[]) IS NULL THEN 0
+            ELSE (toc.data #>> '{jeune_mort}'::text[])::integer
+        END AS count_min,
+        CASE
+            WHEN (toc.data #>> '{adulte}'::text[]) ~~* ''::text OR (toc.data #>> '{adulte}'::text[]) IS NULL THEN 0
+            ELSE (toc.data #>> '{adulte}'::text[])::integer
+        END +
+        CASE
+            WHEN (toc.data #>> '{oeuf}'::text[]) ~~* ''::text OR (toc.data #>> '{oeuf}'::text[]) IS NULL THEN 0
+            ELSE (toc.data #>> '{oeuf}'::text[])::integer
+        END +
+        CASE
+            WHEN (toc.data #>> '{jeune_vivant}'::text[]) ~~* ''::text OR (toc.data #>> '{jeune_vivant}'::text[]) IS NULL THEN 0
+            ELSE (toc.data #>> '{jeune_vivant}'::text[])::integer
+        END +
+        CASE
+            WHEN (toc.data #>> '{jeune_mort}'::text[]) ~~* ''::text OR (toc.data #>> '{jeune_mort}'::text[]) IS NULL THEN 0
+            ELSE (toc.data #>> '{jeune_mort}'::text[])::integer
+        END AS count_max,
+    to2.cd_nom,
+    t.nom_complet AS nom_cite,
+    alt.altitude_min,
+    alt.altitude_max,
+    tbs.geom AS the_geom_4326,
+    st_centroid(tbs.geom) AS the_geom_point,
+    tbs.geom_local AS the_geom_local,
+    tbv.visit_date_min AS date_min,
+    COALESCE(tbv.visit_date_max, tbv.visit_date_min) AS date_max,
+    tbs.id_inventor AS id_digitiser,
+    concat(tr.nom_role, ' ', tr.prenom_role) AS observers,
+    obs.ids_observers[1] AS ids_observers,
+    tbv.comments AS comment_context,
+    to2.comments AS comment_description
+   FROM gn_monitoring.t_base_sites tbs
+     LEFT JOIN gn_monitoring.t_site_complements tsc ON tsc.id_base_site = tbs.id_base_site
+     LEFT JOIN gn_monitoring.t_base_visits tbv ON tbs.id_base_site = tbv.id_base_site
+     LEFT JOIN observers obs ON obs.id_base_visit = tbv.id_base_visit
+     LEFT JOIN gn_monitoring.t_observations to2 ON to2.id_base_visit = tbv.id_base_visit
+     LEFT JOIN gn_monitoring.t_observation_complements toc ON toc.id_observation = to2.id_observation
+     LEFT JOIN utilisateurs.t_roles tr ON tr.id_role = obs.ids_observers[1]
+     LEFT JOIN taxonomie.taxref t ON to2.cd_nom = t.cd_nom
+     LEFT JOIN t_source ON true
+     LEFT JOIN LATERAL ref_geo.fct_get_altitude_intersection(tbs.geom_local) alt(altitude_min, altitude_max) ON true
+     LEFT JOIN gn_commons.t_modules tm ON tm.id_module = tsc.id_module
+  WHERE tm.module_label::text ~~* 'Nichoirs'::text AND to2.cd_nom IS NOT NULL AND to2.uuid_observation IS NOT NULL;
