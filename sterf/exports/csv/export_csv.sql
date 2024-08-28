484fb04a128032daac3f5bed0d714ea04e887109
DonovanMaillard <donovan.maillard@gmail.com>
2024-08-28 16:10:01 +0200

    Add sterf

---
+++ sterf/exports/csv/export_csv.sql
@@ -0,0 +1,9 @@
+------- Export des observations STERF ----------
+
+
+
+---------- Export par transect STERF ----------
+DROP VIEW  IF EXISTS gn_monitoring.v_export_sterf_standard;
+
+CREATE OR REPLACE VIEW gn_monitoring.v_export_sterf_standard AS (
+);
