4552cd7210893f1fba444bfb867195970577c0b1
HURABIELLE Adèle <adele.hurabielle@gmail.com>
2024-09-10 17:40:30 +0200

    POPReptile: MAJ de la vue synthese

--- POPReptile/synthese.sql
+++ POPReptile/synthese.sql
@@ -1,121 +1,140 @@
--- le fichier sera joué à l'installation avec la valeur de module_code qui sera attribué automatiquement
+-- synthese.sql propre au protocole POPReptile.
+-- Ce fichier a été généré à partir d'une copie du fichier synthese_svo.sql du module monitoring
+-- (Vue générique pour alimenter la synthèse dans le cadre d'un protocole site-visite-observation)
 --
---
--- Personalisations possibles
---
---  - ajouter des champs specifiques qui peuvent alimenter la synthese
---      jointure avec les table de complement
---
---  - choisir les valeurs de champs de nomenclatures qui seront propres au modules
-
-
--- ce fichier contient une variable :module_code (ou :'module_code')
+-- Le fichier sera joué à l'installation avec la valeur de module_code qui sera attribué automatiquement
+-- Il contient une variable :module_code (ou :'module_code')
 -- utiliser psql avec l'option -v module_code=<module_code
-
--- ne pas remplacer cette variable, elle est indispensable pour les scripts d'installations
+-- Ne pas remplacer cette variable, elle est indispensable pour les scripts d'installations
 -- le module pouvant être installé avec un code différent de l'original
 
-DROP VIEW IF EXISTS gn_monitoring.v_synthese_POPReptile;
+DROP VIEW IF EXISTS gn_monitoring.v_synthese_:module_code;
 
-CREATE OR REPLACE VIEW gn_monitoring.v_synthese_POPReptile
+CREATE OR REPLACE VIEW gn_monitoring.v_synthese_:module_code
 AS WITH source AS (
-         SELECT id_source
-           FROM gn_synthese.t_sources
-          WHERE name_source = CONCAT('MONITORING_', UPPER(:module_code))
-        )
- SELECT 
+    SELECT
+        id_source
+    FROM gn_synthese.t_sources
+    WHERE name_source = CONCAT('MONITORING_', UPPER(:module_code))
+    LIMIT 1
+),
+sites AS (
+    SELECT
+        id_base_site,
+        base_site_name,
+	altitude_min,
+	altitude_max,
+        geom AS the_geom_4326,
+        ST_CENTROID(geom) AS the_geom_point,
+        geom_local as geom_local
+    FROM gn_monitoring.t_base_sites
+),
+visits AS (
+    SELECT
+        id_base_visit,
+        uuid_base_visit,
+        id_module,
+        id_base_site,
+        id_dataset,
+        id_digitiser,
+        visit_date_min AS date_min,
+	COALESCE (visit_date_max, visit_date_min) AS date_max,
+        comments
+        FROM gn_monitoring.t_base_visits
+),
+observers AS (
+    SELECT
+        array_agg(r.id_role) AS ids_observers,
+        STRING_AGG(CONCAT(r.nom_role, ' ', prenom_role), ' ; ') AS observers,
+        id_base_visit
+    FROM gn_monitoring.cor_visit_observer cvo
+    JOIN utilisateurs.t_roles r
+    ON r.id_role = cvo.id_role
+    GROUP BY id_base_visit
+)
+SELECT
+    -- Champs obligatoires
     o.uuid_observation AS unique_id_sinp,
-    v.uuid_base_visit AS unique_id_sinp_grp,
-    (SELECT id_source FROM source) AS id_source,
+    obs.ids_observers,
+    o.id_observation,
     o.id_observation AS entity_source_pk_value,
+    -- Autres champs
+    v.uuid_base_visit AS unique_id_sinp_grp,
+    source.id_source,
+    v.id_module as id_module,
     v.id_dataset,
-    ref_nomenclatures.get_id_nomenclature('METH_OBS'::character varying, '0'::character varying) AS id_nomenclature_obs_meth, 
-    CASE 
-        WHEN json_extract_path(oc.data::json,'stade_vie')::text = 'Adultes' THEN ref_nomenclatures.get_id_nomenclature('STADE_VIE'::character varying, '2'::character varying)
-        WHEN json_extract_path(oc.data::json,'stade_vie')::text IN ('Nouveaux-nés','Juvéniles') THEN ref_nomenclatures.get_id_nomenclature('STADE_VIE'::character varying, '3'::character varying)
-        ELSE ref_nomenclatures.get_id_nomenclature('STADE_VIE'::character varying, '1'::character varying)
-    END AS id_nomenclature_life_stage,
-    ref_nomenclatures.get_id_nomenclature('SEXE'::character varying, '6'::character varying) AS id_nomenclature_sex,
-    ref_nomenclatures.get_id_nomenclature('OBJ_DENBR'::character varying, 'IND'::character varying) AS id_nomenclature_obj_count,
+    v.id_digitiser,
+    ref_nomenclatures.get_id_nomenclature('NAT_OBJ_GEO', 'In') AS id_nomenclature_geo_object_nature,
+    ref_nomenclatures.get_id_nomenclature('TYP_GRP', 'PASS') AS id_nomenclature_grp_typ,
+    -- Methode d'observation à vue, que ça soit sous plaque ou entre les plaques
+    ref_nomenclatures.get_id_nomenclature('METH_OBS', '0') AS id_nomenclature_obs_technique,
+    -- On ne renseigne pas le bio_status : on n'en dispose pas
+    ref_nomenclatures.get_id_nomenclature('ETA_BIO', '1') as id_nomenclature_bio_condition,
+    -- On ne renseigne pas la naturalness : on n'en dispose pas
+    -- On regarde s'il existe des médias pour transmettre les preuves
     CASE
-        WHEN json_extract_path(oc.data::json,'type_denombrement')::text = 'Compté' THEN ref_nomenclatures.get_id_nomenclature('TYP_DENBR'::character varying, 'Co'::character varying)
-        WHEN json_extract_path(oc.data::json,'type_denombrement')::text = 'Estimé' THEN ref_nomenclatures.get_id_nomenclature('TYP_DENBR'::character varying, 'Es'::character varying)
-        ELSE ref_nomenclatures.get_id_nomenclature('TYP_DENBR'::character varying, 'NSP'::character varying)
-    END AS id_nomenclature_type_count,
-    CASE 
-         WHEN json_extract_path(oc.data::json,'presence')::text = 'Oui' THEN ref_nomenclatures.get_id_nomenclature('STATUT_OBS'::character varying, 'Pr'::character varying) 
-         ELSE ref_nomenclatures.get_id_nomenclature('STATUT_OBS'::character varying, 'No'::character varying)
-    END AS id_nomenclature_observation_status,
-    ref_nomenclatures.get_id_nomenclature('ETAT_BIO'::character varying, '1'::character varying) as id_nomenclature_bio_condition,
-    ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE'::character varying, 'Te'::character varying) AS id_nomenclature_source_status,
-    ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO'::character varying, '1'::character varying) AS id_nomenclature_info_geo_type,
+	WHEN unique_id_media IS NOT NULL THEN ref_nomenclatures.get_id_nomenclature('PREUVE_EXIST', '1')
+	ELSE ref_nomenclatures.get_id_nomenclature('PREUVE_EXIST', '2')
+    END AS id_nomenclature_exist_proof,
+    ref_nomenclatures.get_id_nomenclature('OBJ_DENBR', 'IND') AS id_nomenclature_obj_count,
+    nullif(json_extract_path(oc.data::json,'id_nomenclature_typ_denbr')::text, 'null')::integer as id_nomenclature_type_count,
     CASE
-        WHEN json_extract_path(oc.data::json,'type_denombrement')::text = 'Compté' AND nullif(json_extract_path(oc.data::json,'nombre_compte')::text,null)::integer IS NOT NULL THEN nullif(json_extract_path(oc.data::json,'nombre_compte')::text,null)::integer
-        WHEN json_extract_path(oc.data::json,'type_denombrement')::text = 'Estimé' AND nullif(json_extract_path(oc.data::json,'nombre_estime_min')::text,null)::integer IS NOT NULL THEN nullif(json_extract_path(oc.data::json,'nombre_estime_min')::text,null)::integer
-        ELSE 1
-    END AS count_min,
-    CASE
-        WHEN json_extract_path(oc.data::json,'type_denombrement')::text = 'Compté' AND nullif(json_extract_path(oc.data::json,'nombre_compte')::text,null)::integer IS NOT NULL THEN nullif(json_extract_path(oc.data::json,'nombre_compte')::text,null)::integer
-        WHEN json_extract_path(oc.data::json,'type_denombrement')::text = 'Estimé' AND nullif(json_extract_path(oc.data::json,'nombre_estime_max')::text,null)::integer IS NOT NULL THEN nullif(json_extract_path(oc.data::json,'nombre_estime_max')::text,null)::integer
-        ELSE 1
-    END AS count_max,
+	WHEN oc.DATA->>'presence' = 'Non' THEN ref_nomenclatures.get_id_nomenclature('STATUT_OBS', 'No')
+	ELSE ref_nomenclatures.get_id_nomenclature('STATUT_OBS', 'Pr')
+    END as id_nomenclature_observation_status,
+    ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE', 'Te') AS id_nomenclature_source_status,
+    ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO', '1') AS id_nomenclature_info_geo_type,
+    nullif(json_extract_path(oc.data::json,'id_nomenclature_stade')::text, 'null')::integer AS id_nomenclature_life_stage,
+    nullif(json_extract_path(oc.data::json,'id_nomenclature_sex')::text, 'null')::integer AS id_nomenclature_sex,
+    nullif(json_extract_path(oc.data::json,'count_min')::text,'null')::integer AS count_min,
+    nullif(json_extract_path(oc.data::json,'count_max')::text,'null')::integer AS count_max,
     o.cd_nom,
     t.nom_complet AS nom_cite,
     s.altitude_min,
     s.altitude_max,
-    s.geom AS the_geom_4326,
-    st_centroid(s.geom) AS the_geom_point,
+    -- Normalement, pas besoin d'adapter en fonction du système de projection du module monitoring et de la synthese car ils sont sensés être cohérents.
+    -- Par défaut, geom_local en lambert93 et geom en WGS84
+    s.the_geom_4326,
+    s.the_geom_point,
     s.geom_local AS the_geom_local,
-    v.visit_date_min AS date_min,
-    v.visit_date_min AS date_max,
+    -- FIXME: on pourrait ajouter les horaires de visite (champs complémentaires si remplis)
+    v.date_min,
+    v.date_max,
     obs.observers,
-    v.id_digitiser,
-    ref_nomenclatures.get_id_nomenclature('METH_DETERMIN'::character varying, '0'::character varying) AS id_nomenclature_determination_method,
-    v.id_module as id_module,
+    -- Examen visuel à "distance" (sans manipulation des individus)
+    ref_nomenclatures.get_id_nomenclature('METH_DETERMIN', '18') AS id_nomenclature_determination_method,
     v.comments AS comment_context,
     o.comments AS comment_description,
-    obs.ids_observers,
+    -- Colonnes complémentaires qui ont leur utilité dans la fonction synthese.import_row_from_table
     v.id_base_site,
-    v.id_base_visit, 
+    v.id_base_visit,
+    -- Informations complémentaires à conserver
     json_build_object(
-        'expertise_operateur', json_extract_path(tsg.data::json,'expertise')::text, 
-        'nom_aire', tsg.sites_group_name, 
-        'description_aire', tsg.sites_group_description, 
-        'habitat_principal_aire', json_extract_path(tsg.data::json,'habitat_principal')::text, 
-        'commentaire_aire', tsg.comments, 
-        'nom_transect', s.base_site_name, 
-        'methode_prospection', json_extract_path(sc.data::json,'methode_prospection')::text, 
-        'materiaux_plaques', json_extract_path(sc.data::json,'type_materiaux')::text, 
-        'nombre_plaques', json_extract_path(sc.data::json,'nb_plaques')::text, 
-        'milieu_transect', json_extract_path(sc.data::json,'milieu_transect')::text, 
-        'milieu_bordier', json_extract_path(sc.data::json,'milieu_bordier')::text, 
-        'milieu_mosaique', json_extract_path(sc.data::json,'milieu_mosaique_vegetale')::text, 
-        'milieu_homogene', json_extract_path(sc.data::json,'milieu_homogene')::text, 
-        'milieu_anthropique', json_extract_path(sc.data::json,'milieu_anthropique')::text, 
-        'precision_milieu_anthropique', json_extract_path(sc.data::json,'milieu_transect_autre')::text, 
-        'microhab_favorable', json_extract_path(sc.data::json,'microhabitat_favorable')::text, 
-        'frequentation_humaine_transect', json_extract_path(sc.data::json,'frequentation_humaine')::text, 
-        'commentaire_transect', json_extract_path(sc.data::json,'comment')::text, 
-        'num_passage', json_extract_path(vc.data::json,'num_passage')::text, 
-        'heure_debut', json_extract_path(vc.data::json,'Heure_debut')::text, 
-        'heure_fin', json_extract_path(vc.data::json,'Heure_fin')::text, 
-        'meteo_visite', json_extract_path(vc.data::json,'meteo')::text, 
-        'vent_visite', json_extract_path(vc.data::json,'vent')::text, 
-        'abondance', json_extract_path(oc.data::json,'abondance')::text
+        'methode_prospection', json_extract_path(vc.DATA::json, 'methode_prospection'),
+        'expertise_operateur', json_extract_path(vc.DATA::json,'expertise'),
+        'nom_aire', tsg.sites_group_name,
+        'description_aire', tsg.sites_group_description,
+        'habitat_principal_aire', json_extract_path(tsg.data::json,'habitat_principal')::text,
+        'commentaire_aire', tsg.comments,
+        'nom_transect', s.base_site_name,
+        'accessibilite', json_extract_path(vc.DATA::json, 'accessibility'),
+        'etat_site', json_extract_path(vc.DATA::json, 'etat_site'),
+        'date_changement_etat_site', json_extract_path(vc.DATA::json, 'date_changement_etat_site'),
+        'milieu_transect', json_extract_path(sc.data::json,'milieu_transect')::text,
+        'num_passage', json_extract_path(vc.data::json,'num_passage')::text,
+        'heure_debut', json_extract_path(vc.data::json,'Heure_debut')::text,
+        'heure_fin', json_extract_path(vc.data::json,'Heure_fin')::text
         ) as additional_data
-   FROM gn_monitoring.t_base_visits v
-     JOIN gn_monitoring.t_visit_complements vc on v.id_base_visit = vc.id_base_visit 
-     JOIN gn_monitoring.t_base_sites s ON s.id_base_site = v.id_base_site
-     JOIN gn_monitoring.t_site_complements sc on sc.id_base_site = s.id_base_site
-     JOIN gn_monitoring.t_sites_groups tsg ON sc.id_sites_group = tsg.id_sites_group
-     JOIN gn_commons.t_modules m ON m.id_module = v.id_module
-     JOIN gn_monitoring.t_observations o ON o.id_base_visit = v.id_base_visit
-     JOIN gn_monitoring.t_observation_complements oc ON oc.id_observation = o.id_observation
-     JOIN taxonomie.taxref t ON t.cd_nom = o.cd_nom
-     LEFT JOIN LATERAL ( SELECT array_agg(r.id_role) AS ids_observers,
-            string_agg(concat(r.nom_role, ' ', r.prenom_role), ' ; '::text) AS observers
-           FROM gn_monitoring.cor_visit_observer cvo
-             JOIN utilisateurs.t_roles r ON r.id_role = cvo.id_role
-          WHERE cvo.id_base_visit = v.id_base_visit) obs ON true 
-    WHERE m.module_code = :module_code;
-
+FROM gn_monitoring.t_observations o
+JOIN gn_monitoring.t_observation_complements oc USING(id_observation)
+JOIN visits v USING (id_base_visit)
+JOIN gn_monitoring.t_visit_complements vc USING (id_base_visit)
+JOIN sites s USING (id_base_site)
+JOIN gn_monitoring.t_site_complements sc USING (id_base_site)
+JOIN gn_monitoring.t_sites_groups tsg USING (id_sites_group)
+JOIN gn_commons.t_modules m ON m.id_module = v.id_module
+JOIN taxonomie.taxref t USING (cd_nom)
+JOIN source ON TRUE
+JOIN observers obs USING (id_base_visit)
+LEFT JOIN gn_commons.t_medias tm ON (tm.id_table_location = gn_commons.get_table_location_id('gn_monitoring', 't_observations') AND tm.uuid_attached_row = o.uuid_observation)
+WHERE m.module_code = :module_code;
