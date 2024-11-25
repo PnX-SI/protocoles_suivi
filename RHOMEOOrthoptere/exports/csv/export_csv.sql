b190fb846b6ca8581bf512c184e7edde1b3f7efa
Amandine <amandine.sahl@gmail.com>
2024-11-25 10:58:47 +0100

    Update export_csv.sql

--- RHOMEOOrthoptere/exports/csv/export_csv.sql
+++ RHOMEOOrthoptere/exports/csv/export_csv.sql
@@ -1,5 +1,3 @@
- alter table gn_monitoring.t_base_sites alter column id_nomenclature_type_site drop not null;
-
 -------------------------------------------------final --rhomeoothoptere standard------------------------------------------
 -- View: gn_monitoring.v_export_rhomeoothoptere_standard
 
