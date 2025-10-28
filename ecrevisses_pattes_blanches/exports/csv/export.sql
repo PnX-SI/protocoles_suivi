19ea4de71ad4680b177b3cdde4cfd56770e29cf4
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2025-10-28 09:50:17 +0100

    Ecrevisses correction export

--- ecrevisses_pattes_blanches/exports/csv/export.sql
+++ ecrevisses_pattes_blanches/exports/csv/export.sql
@@ -84,7 +84,7 @@ AS WITH e_patte_blanche AS (
              LEFT JOIN gn_monitoring.t_observation_complements toc ON toc.id_observation = obs.id_observation
              LEFT JOIN taxonomie.taxref t ON t.cd_nom = obs.cd_nom
              LEFT JOIN ref_nomenclatures.t_nomenclatures tn ON ((toc.data ->> 'id_nomenclature_statut_observation'::text)::integer) = tn.id_nomenclature
-          WHERE m_1.module_code::text = 'ecrevisses_pattes_blanches'::text AND t.cd_nom = 853999
+          WHERE m_1.module_code::text = 'ecrevisses_pattes_blanches'::text AND t.cd_nom = 162667
         )
  SELECT s.base_site_code AS code_site,
     s.base_site_name AS nom_site,
@@ -98,13 +98,13 @@ AS WITH e_patte_blanche AS (
     tvc.data ->> 'time_start'::text AS heure_debut,
     tvc.data ->> 'time_end'::text AS heure_fin,
     tvc.data ->> 'substrats_majoritaires'::text AS substrats_majoritaires,
-    tvc.data ->> 'habitats_aquatiques'::text AS habitats_aquatiques,  
-    tvc.data ->> 'periode'::text AS periode, 
-    tvc.data ->> 'colmatage'::text AS colmatage, 
+    tvc.data ->> 'habitats_aquatiques'::text AS habitats_aquatiques,
+    tvc.data ->> 'periode'::text AS periode,
+    tvc.data ->> 'colmatage'::text AS colmatage,
     tvc.data ->> 'organisme'::text AS organisme,
     tvc.data ->> 'ripisylve'::text AS ripisylve,
-    tvc.data ->> 'technique'::text AS technique, 
-    tvc.data ->> 'lineaire_assec_m'::text AS lineaire_assec_m, 
+    tvc.data ->> 'technique'::text AS technique,
+    tvc.data ->> 'lineaire_assec_m'::text AS lineaire_assec_m,
     tvc.data ->> 'lineaire_prospection'::text AS lineaire_prospection,
       tvc.data ->> 'conditions_d_observation'::text AS conditions_d_observation,
     tvc.data ->> 'largeur_du_cours_d_eau_m'::text AS largeur_du_cours_d_eau_m,
